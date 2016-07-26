2ba23cd8e6f53739dc42fb6091edce8c9e9c2022
carles molins duran <carles molins duran>
2016-07-26 16:53:16 +0200

    Updated Kp and Kv values for a more stable controller

--- runsim.m
+++ runsim.m
@@ -7,7 +7,7 @@ addpath('trajectories');
 controlhandle = @controller;
 
 % Choose which trajectory you want to test with
-%trajhandle = @traj_diamond;
- trajhandle = @traj_line;
+trajhandle = @traj_diamond;
+% trajhandle = @traj_line;
 
 [t, state] = simulation_2d(controlhandle, trajhandle);
